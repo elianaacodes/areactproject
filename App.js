eae26b771688623ed833c73b183b27188f97660a
Anna <123110972+elianaacodes@users.noreply.github.com>
2023-05-15 16:53:15 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import {Carinventory} from './Carinventory'
+
+function App () {
+  return (
+     <Carinventory />
+  );
+}
+
+export default App;
---
+++ Carinventory.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Carinventory() {
+  return (
+    <div> This is my car inventory.</div>
+  )
+}
